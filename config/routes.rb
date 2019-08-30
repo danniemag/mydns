a4f0125c0f2d648a4cf85e3dc64efb91adfe30c1
Dannie Magalhaes <seliksy@gmail.com>
2019-08-29 21:59:08 -0300

    Fix routing to better reflect relation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :records
-  resources :domains
+  resources :domains do
+    resources :records
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
